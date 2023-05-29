b4f0bb9511a1c7ee09e77596dde99b0392dcf90d
Markus Byremo <markuswb77@gmail.com>
2023-05-29 09:32:12 +0200

    Implemented small list of popular recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,23 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Popular() {
 
-  /*
+
+  const [popular, setPopular] = useState([]);
+
+
+  
   useEffect(() => {
     getPopular();
   },[])
- 
-  */
+  
+
  
   const getPopular  = async () => {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=8`);
 
     const data = await api.json();
-
+    setPopular(data.recipes)
+    
     console.log(data);
-  }
-   return <div>Popular</div>;
+  };
 
+   return (
+   <div>
+   
+    {popular.map((recipe) => {
+      return(
+       <div key={recipe.id}>
+          <p>{recipe.title}</p>
+       </div>
+      );
+    })}
+   
+   </div>
+   );
 }
 
-export default Popular
+export default Popular 
