fdc8ec875d7ab26e99520b6f1065d8e73992d1e6
Markus Byremo <markuswb77@gmail.com>
2023-06-01 06:38:22 +0200

    added styling for cards and changed options for slideshow

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -7,11 +7,10 @@ function Popular() {
 
   const [popular, setPopular] = useState([]);
 
-  
+
   useEffect(() => {
     getPopular();
   },[])
-
 
  
   const getPopular  = async () => {
@@ -28,14 +27,26 @@ function Popular() {
 
           <Wrapper>
             <h3>Popular Picks</h3>
+            
+            <Splide
+              options={{
+              perPage: 3,
+              arrows: false,
+              pagination: false,
+              drag: "free",
+              gap: "5rem"
 
-            <Splide>
+              }}>
+
               {popular.map((recipe) => {
                 return(
-                  <SplideSlide>
+                  <SplideSlide
+                  key={recipe.id}
+                  >
                     <Card key={recipe.id}>
                       <p>{recipe.title}</p>
                       <img src={recipe.image} alt={recipe.title} />
+                      <Gradient />
                     </Card>
                   </SplideSlide>
                 );
@@ -54,11 +65,40 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
 
   img{
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%,0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;'
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.5));
+`;
 
 export default Popular 
