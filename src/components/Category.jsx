e21f093a193edf9eab3725929605fcc4e09edd3d
Markus Byremo <markuswb77@gmail.com>
2023-06-14 22:06:22 +0200

    added Cuisine page and added it to router

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,37 +1,84 @@
 import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
 import {GiNoodles, GiChopsticks} from "react-icons/gi";
 import styled from "styled-components";
+import {NavLink} from 'react-router-dom';
+
 
 
 function Category() {
     
   return (
+
     <List>
-        <div>
+    <div>
+    <FaPizzaSlice />
+        <h4>
+            Italian
+        </h4>
+    </div>
+
+    <div>
+    <FaHamburger />
+        <h4>
+        American
+
+        </h4>
+    </div>
+
+    <div>
+    <GiNoodles />
+        <h4>
+        Thai
+
+        </h4>
+    </div>
+
+    <div>
+    <GiChopsticks />
+        <h4>
+        Japanese
+
+        </h4>
+    </div>
+
+</List>
+
+
+    /*
+    <List>
+        <NavLink to={'cuisine/Italian'}>
             <FaPizzaSlice />
             <h4>
                 Italian
             </h4>
-        </div>
-        <div>
-            <FaHamburger />
+        </NavLink>
+
+        <NavLink to={'cuisine/American'}>
+        <FaHamburger />
             <h4>
                 American
             </h4>
-        </div>
-        <div>
-            <GiNoodles />
+        </NavLink>
+
+
+        <NavLink to={'cuisine/Thai'}>
+        <GiNoodles />
             <h4>
                 Thai
             </h4>
-        </div>
-        <div>
-            <GiChopsticks />
+        </NavLink>
+        
+        
+        <NavLink to={'cuisine/Japanese'}>
+        <GiChopsticks />
             <h4>
                 Japanese
             </h4>
-        </div>
+        </NavLink>
+
+
     </List>
+    */
   )
 }
 
