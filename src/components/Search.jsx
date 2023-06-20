9e31ba5eb72c16f47979a959ffc6571c0074d58e
Markus Byremo <markuswb77@gmail.com>
2023-06-20 20:22:02 +0200

    added new searched page with the react (useNavigate)

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components'
 import { useState } from 'react'
 import {FaSearch} from 'react-icons/fa'
+import {useNavigate} from 'react-router-dom'
 
 
 function Search() {
 
     const [input,setInput] = useState("");
+    const navigate = useNavigate();
 
     const submitHandler = (e) => {
         e.preventDefault();
-        
-    }
-
-
+        navigate("/searched/" + input);
+    };
 
   return (
     <FormStyle onSubmit={submitHandler}>
---
+++ src/pages/Searched.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Searched() {
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default Searched
