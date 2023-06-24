e5b0e20e38b0172882e8a76498a179bc36000ee0
Markus Byremo <markuswb77@gmail.com>
2023-06-24 08:01:58 +0200

    added the fetch method for searched query

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Searched() {
-  return (
-    <div>Hello</div>
-  )
+
+    const [searchedRecipes, setSearchedRecipes] = useState([]);
+    let params = useParams();
+
+
+
+    useEffect(() => {
+        getSearched(params.search);
+      },[params.search]);
+
+    const getSearched  = async (name) => {
+
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`);
+        
+        const recipes = await data.json();
+        setSearchedRecipes(recipes.results);
+    };
+
+
+    return (
+        <div>Searched</div>
+    )
 }
+
+
 
 export default Searched
