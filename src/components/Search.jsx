583b3c1e038d3edde1313107010af25d4b406b73
Markus Byremo <markuswb77@gmail.com>
2023-06-25 08:34:43 +0200

    styling for Recipe page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -39,7 +39,7 @@ const FormStyle = styled.form`
     }
 
     input{
-        border: mone;
+        border: none;
         background: linear-gradient(35deg, #494949,#313131);
         font-size: 1.5rem;
         color: white;
--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -4,9 +4,50 @@ import { useParams } from "react-router-dom"
 import React from "react"
 
 function Recipe() {
+
+  let params = useParams();
+  const [details, setDetails] = useState({});
+
+
+  useEffect(() => {
+    FetchDetails();
+  },[params.name]);
+
+  const FetchDetails = async () => {
+
+    const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`);
+    const detailData = await data.json();
+
+    setDetails(detailData);
+  }
+
+
   return (
-    <div>Recipe</div>
+    <div>{details.title}</div>
   )
 }
 
-export default Recipe
+const DetailWrapper = styled.div`
+    margin-top: 10rem;
+    margin-bottom: 5rem;
+    display: flex;
+    .active{
+        background: linear-gradient(35deg, #494949,#313131);
+        color: white;
+    }
+        h2{
+            margin-bottom: 2rem;
+        }
+
+        li{
+        font-size: 1.2rem;
+        line-height: 2.5rem;
+        }
+
+        ul{
+        margin-top: 2rem;
+        }
+`;
+
+
+export default Recipe   
