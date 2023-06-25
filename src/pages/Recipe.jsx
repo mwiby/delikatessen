3e824a81e864b0de1f660db7ddc61f5c2eaabeec
Markus Byremo <markuswb77@gmail.com>
2023-06-25 20:06:03 +0200

    Added Summary and inst to the Recipe page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -20,6 +20,7 @@ function Recipe() {
     const detailData = await data.json();
 
     setDetails(detailData);
+    console.log(detailData);
   }
 
 
@@ -42,6 +43,10 @@ function Recipe() {
             >
                 Ingredients
             </Button>
+            <div>
+                <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+                <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+            </div>
         </Info>
     </DetailWrapper>
   )
